17a1412cba1d0e055774e9d6a6b6b9b3b747063e
Valek <ziryanovvalentin@mail.ru>
2022-07-06 00:15:23 +0300

    Auth

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const needle = require('needle')
 const cheerio = require('cheerio')
 
 const URL = 'https://lafounder.com/article/motivaciya-citaty'
+let store = []
 
 needle.get(URL, function (err, res) {
   if (err) throw err
   const $ = cheerio.load(res.body)
-  $('div.field--name-field-note-before')
-    .find('p')
-    .each((i, elem) => {
-      console.log(elem.children[0].data)
-    })
+  let instance = $('div.field--name-field-note-before').find('p')
+  instance.each((i, elem) => {
+    store.push(elem.children[0].data)
+  })
+  instance.find('strong').each((iAuth, elemAuth) => {
+    console.log(store[iAuth - 1] + elemAuth.children[0].data)
+    console.log(iAuth)
+  })
 })
